3976620e1fe9dbf622a4ff39e35f8095357ae5e9
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2022-07-18 15:39:01 +0300

    instagram logo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>this is instagram clone using react</h1>
+      <div className="app_header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+        <form>
+          <input className = "app_headerSearch" type="text" placeholder="search" />
+        </form>
+      </div>
     </div>
   );
 }
